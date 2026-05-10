adb7fc052c13f750a104691b28eafebb4a26b950
Josh Bonnick <jbonnick@acquiredigital.com>
2026-05-10 14:25:21 +0100

    add cooldown mechanic to player shooting with new constant and logic

--- player.py
+++ player.py
@@ -4,6 +4,8 @@ from constants import *
 from shot import Shot
 
 class Player(CircleShape):
+    shot_cooldown = 0
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
 
@@ -21,6 +23,8 @@ class Player(CircleShape):
         return [a, b, c]
 
     def update(self, dt):
+        self.shot_cooldown -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -41,6 +45,11 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def shoot(self):
+        if self.shot_cooldown > 0:
+            return None
+
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
