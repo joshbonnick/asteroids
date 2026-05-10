d21d538ca7644065be5d9722b6528d9c25fbf878
Josh Bonnick <jbonnick@acquiredigital.com>
2026-05-10 12:48:20 +0100

    add logging and basic game loop setup

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pygame
-
+from logger import log_state
 from constants import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+        screen.fill((0,0,0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
